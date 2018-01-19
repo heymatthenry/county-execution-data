2981311dd50b82bd6555e36fde43d24973961487
Matt Henry <matthenry@fastmail.fm>
2018-01-19 16:33:58 -0500

    Remove 'City' from names of cities masquerading as counties (i.e. in VA); Remove federal executions

--- process.R
+++ process.R
@@ -1,11 +1,14 @@
 library(dplyr)
+library(stringr)
 library(lubridate)
 
 executions <- read.csv("data/execution_database.csv", stringsAsFactors = FALSE)
 executions$Date <- as.Date(executions$Date, format = "%m/%d/%Y")
 
 county_execution_counts <- executions %>%
-                            mutate(Year = year(Date), county = County, state = State) %>%
-                            count(County, State, Year) %>%
-                            arrange(Year) %>%
-                            write.csv("data/county_execution_counts.csv", row.names = FALSE)
+  mutate(Year = year(Date), county = County, state = State) %>%
+  filter(State != "FE") %>%
+  mutate_at(.vars = vars(County), .funs = funs(str_replace(County, " City", ""))) %>%
+  count(County, State, Year) %>%
+  arrange(Year) %>%
+  write.csv("data/county_execution_counts.csv", row.names = FALSE)
